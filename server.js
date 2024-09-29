8033a12a73bc94c5dc2cf160ebcb0aa247bcf0f0
adelssss <166361844+adelssss@users.noreply.github.com>
2024-09-29 21:39:19 +0500

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const EventEmitter = require('events'); 
 const path = require('path'); 
+
 const app = express(); 
 const server = http.createServer(app);
 const eventEmitter = new EventEmitter(); 
@@ -19,6 +20,7 @@ app.get('/json', (req, res) => {
         numbers: [1, 2, 3]
     });
 });
+
 app.get('/echo', (req, res) => {
     const message = req.query.message || ''; 
     const response = {
@@ -35,10 +37,9 @@ app.get('/chat', (req, res) => {
     eventEmitter.emit('message', message); 
     res.send('Message sent'); 
 });
-
 // Sets up a Server-Sent Events (SSE) connection for real-time updates
 app.get('/sse', (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Content-Type', 'text/event-stream'); 
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
 
@@ -47,13 +48,12 @@ app.get('/sse', (req, res) => {
     };
     eventEmitter.on('message', sendMessage);
 
-
     req.on('close', () => {
         eventEmitter.removeListener('message', sendMessage); 
         res.end();
     });
 });
-
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`); 
 });
+
